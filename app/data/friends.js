15af87648d1c0cf10fd3fbeddc4dcb575d4f3c7b
Test123 <Test123@gavins-mbp.home>
2019-10-22 04:09:11 -0400

    <all buttons working>

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+var friends = [
+
+    {
+        name:"Toni_Braxton",
+        photo:"app/images/IMG_2687.JPG",
+        scores:[
+            "5",
+            "3",
+            "2",
+            "4",
+            "5",
+            "5",
+            "2",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    
+    {
+        name:"Sade",
+        photo:"app/images/IMG_2441 2.JPG",
+        scores:[
+            "5",
+            "1",
+            "5",
+            "4",
+            "5",
+            "1",
+            "3",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+
+    {
+        name:"Anita_Baker",
+        photo:"app/images/IMG_2420 2.JPG",
+        scores:[
+            "3",
+            "1",
+            "4",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+
+    {
+        name:"Whitney_Houston",
+        photo:"app/images/IMG_2221.JPG",
+        scores:[
+            "5",
+            "4",
+            "4",
+            "2",
+            "5",
+            "4",
+            "2",
+            "5",
+            "4",
+            "3"
+        ]
+    },
+
+    {
+        name:"Halle_Berry",
+        photo:"app/images/IMG_2137.JPG",
+        scores:[
+            "1",
+            "1",
+            "4",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "4",
+            "5"
+        ]
+    }
+];    
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,77 +1,35 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
-
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
-var path = require("path");
+var bodyParser = require("body-parser");
 
-var bodyParser = require('body-parser');
+// create application/json parser
+var jsonParser = bodyParser.json();
  
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
- 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
  
 // parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
  
 // parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
-// Data
-// ===========================================================
+app.use(bodyParser.text({ type: 'text/html' }));
 
-var friends= [
-    {
-    routeName:  "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-    },
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
-    {
-    routeName:  "darthMaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-    }
-];
+// Data:  app/data/friends.js
+// =================================================================
 
-// Routes
-// ===========================================================
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
 
-app.get("/api/:friends?", function(req, res) {
-  var chosen = req.params.friends;
-  
-  if (chosen) {
-    
-    console.log(chosen);
-
-    for (var i=0; i<friends.length; i++){
-      if (chosen===friends[i].routeName) {
-        res.json(friends[i]);
-        return;
-      }
-     }
-
-    res.send("No friends found");
-
-} else {
-  res.json(friends);
-}
-
-});
-
+// Routes:  app/routing/apiRoutes.js && app/routing/htmlRoutes.js
+// =================================================================
 
 
 // Listener
